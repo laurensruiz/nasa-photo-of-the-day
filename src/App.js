0f2ecf47894c4e114ada04aed41bb8ff55d90a1d
laurensruiz <laurenruiz97@yahoo.com>
2022-03-24 17:00:39 -0400

    reverted to old code for single day

--- src/App.js
+++ src/App.js
@@ -4,35 +4,27 @@ import "./App.css";
 import NasaPhoto from "./components/NasaPhoto";
 import NasaHeader from "./components/NasaHeader";
 
-//let dates =['']
-let endpoints= [
-  'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-01-01',
-  'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2013-01-01',
-  'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2014-02-01'
-]
 
 function App() {
   const [data, setData] = useState([]) 
 
-  //useEffect(() => {
-    axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
-    //.then(res =>{
-      
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(res =>{
       // console.log(res); remember to see what info you have!
-      //setData(res.data); //console.log data wont return anything here
-      .then((data) => console.log(data))
-    }
-    //.catch(err => 
-     // console.error(err))
-  //}, [])
-/*
+      setData(res.data); //console.log data wont return anything here
+    })
+    .catch(err => 
+      console.error(err))
+  }, [])
+
   return (
     <div className="App">
-    {data && <NasaHeader info={data}/>} {/*No need to put 'data && if u put empty array in useState but if type useState() you have to state data first *///}
-    /*{data && <NasaPhoto info={data}/>} {/* make sure to console log data here and not in useeffect to see it*///}
-    /*</div>
+    {data && <NasaHeader info={data}/>} {/*No need to put 'data && if u put empty array in useState but if type useState() you have to state data first */}
+    {data && <NasaPhoto info={data}/>} {/* make sure to console log data here and not in useeffect to see it*/}
+    </div>
   );
-} */
+}
 
 export default App;
 
